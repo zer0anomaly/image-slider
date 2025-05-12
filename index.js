ebc256ec8da231353ddae3b9fac1b6024a51382d
zer0anomaly <zeroanomale@gmail.com>
2025-05-12 15:11:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const backbutton = document.getElementById('backbutton')
 const seconddiv = document.getElementById('seconddiv')
 
 const list = [
-	"images/one.png",
-	"images/two.png"
+	"images/imageone.png",
+	"images/imagetwo.png",
+	"images/imagethree.png"
 ];
 
-currentIndex = 0;
+let currentIndex = 0;
+let currentlength = list.length;
 
 function firstfunction () {
 seconddiv.style.backgroundImage = `url(${list[currentIndex]})`;
@@ -16,5 +18,20 @@ firstfunction()
 
 
 nextbutton.addEventListener("click", () => {
-
+	currentIndex = (currentIndex + 1) % list.length
+	firstfunction()
 })
+backbutton.addEventListener("click", () => {
+	currentIndex = (currentIndex - 1 + list.length) % list.length
+	firstfunction()
+})
+function secondfunction() {
+	if (currentIndex < currentlength -1 ){
+		currentIndex++
+		firstfunction()
+	}else if (currentIndex >= currentlength - 1){
+		currentIndex = 0
+		firstfunction()
+	}
+}
+setInterval(secondfunction, 1000)
